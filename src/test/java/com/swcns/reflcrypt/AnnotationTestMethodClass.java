88f1796c0c403d95abf7f6ac16b20c6d4404aa5c
VINTO1819 <develretr0@gmail.com>
2022-08-26 12:05:19 +0900

    테스트 내용 추가

--- src/test/java/com/swcns/reflcrypt/AnnotationTestMethodClass.java
+++ src/test/java/com/swcns/reflcrypt/AnnotationTestMethodClass.java
@@ -2,10 +2,12 @@ package com.swcns.reflcrypt;
 
 import com.swcns.reflcrypt.annotation.*;
 import com.swcns.reflcrypt.util.ObjectEncryptor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class AnnotationTestMethodClass {
@@ -34,13 +36,13 @@ public class AnnotationTestMethodClass {
 
     @EncryptParams
     void encryptedInstanceParams(@SecurityParam String encryptedField1, @SecurityParam byte[] encryptedField2,
-                                 int nonEncryptedField1, String nonEncryptedField2) {
-        System.out.printf("Encrypted: %s, %s, %d, %s\n", encryptedField1, Arrays.toString(encryptedField2), nonEncryptedField1, nonEncryptedField2);
+                                 int nonEncryptedField1, String nonEncryptedField2, @SecurityParam List<String> encryptedCollection) {
+        System.out.printf("Encrypted: %s, %s, %d, %s, %s\n", encryptedField1, Arrays.toString(encryptedField2), nonEncryptedField1, nonEncryptedField2, encryptedCollection.toString());
     }
 
     @DecryptParams
     void decryptedInstanceParams(@SecurityParam String encryptedField1, @SecurityParam byte[] encryptedField2,
-                                 int nonEncryptedField1, String nonEncryptedField2) {
-        System.out.printf("Decrypted: %s, %s, %d, %s\n", encryptedField1, Arrays.toString(encryptedField2), nonEncryptedField1, nonEncryptedField2);
+                                 int nonEncryptedField1, String nonEncryptedField2, @SecurityParam List<String> encryptedCollection) {
+        System.out.printf("Decrypted: %s, %s, %d, %s, %s\n", encryptedField1, Arrays.toString(encryptedField2), nonEncryptedField1, nonEncryptedField2, encryptedCollection.toString());
     }
 }
--- src/test/java/com/swcns/reflcrypt/AnnotationsTest.java
+++ src/test/java/com/swcns/reflcrypt/AnnotationsTest.java
@@ -7,9 +7,13 @@ import com.swcns.reflcrypt.util.ObjectEncryptor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = { ReflcryptAutoConfiguration.class, TestBeanConfiguration.class })
+import java.util.Collection;
+import java.util.List;
+
+@SpringBootTest(classes = { ReflcryptAutoConfiguration.class, TestBeanConfiguration.class, AopAutoConfiguration.class })
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AnnotationsTest {
     @Autowired
@@ -36,7 +40,17 @@ public class AnnotationsTest {
         System.out.print("평문: ");
         System.out.println(testMethodClass.decryptReturns(encrypted));
 
-        testMethodClass.encryptedInstanceParams(normal.getEncryptedField1(), normal.getEncryptedField2(), normal.getNonEncryptedField1(), normal.getNonEncryptedField2());
-        testMethodClass.decryptedInstanceParams(encrypted.getEncryptedField1(), encrypted.getEncryptedField2(), encrypted.getNonEncryptedField1(), encrypted.getNonEncryptedField2());
+        testMethodClass.encryptedInstanceParams(normal.getEncryptedField1(), normal.getEncryptedField2(), normal.getNonEncryptedField1(), normal.getNonEncryptedField2(), List.of(
+                normal.getEncryptedField1(), "wow"
+        ));
+        testMethodClass.decryptedInstanceParams(encrypted.getEncryptedField1(), encrypted.getEncryptedField2(), encrypted.getNonEncryptedField1(), encrypted.getNonEncryptedField2(), List.of(
+                encrypted.getEncryptedField1(), encryptor.getEncryptedObject("wow")
+        ));
+    }
+
+    @Test
+    void checkType() {
+        List<String> type = List.of("asdf", "1234");
+        System.out.println(Collection.class.isAssignableFrom(type.getClass()));
     }
 }
